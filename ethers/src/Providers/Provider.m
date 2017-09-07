a5ec79317988218f788af9aa3c022be814f086cd
Richard Moore <me@ricmoo.com>
2017-09-07 14:50:13 -0400

    Support mainnet for ENS in provider.

--- ethers/src/Providers/Provider.m
+++ ethers/src/Providers/Provider.m
@@ -245,7 +245,11 @@ static NSString *UserAgent = nil;
             [data appendData:[SecureData hexStringToData:@"0x0178b8bf"]];   // resolver(bytes32)
             [data appendData:nodehash.data];
             
-            getResolverTransaction.toAddress = [Address addressWithString:@"0x112234455c3a32fd11230c42e7bccd4a84e02010"];
+            if (self.testnet) {
+                getResolverTransaction.toAddress = [Address addressWithString:@"0x112234455c3a32fd11230c42e7bccd4a84e02010"];
+            } else {
+                getResolverTransaction.toAddress = [Address addressWithString:@"0x314159265dd8dbb310642f98f50c066173c1259b"];
+            }
             getResolverTransaction.data = data.data;
         }
         [[self call:getResolverTransaction] onCompletion:^(DataPromise *resolverPromise) {
